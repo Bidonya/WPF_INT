af1eae07c03cc89d9d16ac42dbf07442cb548824
Bidonya <101891224+Bidonya@users.noreply.github.com>
2022-03-19 01:03:31 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Integral
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calc(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("\tInput:\nStart: "+start.Text+";\nEnd: "+end.Text+";\nSteps: "+steps.Text+";\nMethod: "+Методы.Text+".", "Info",MessageBoxButton.OK,MessageBoxImage.Asterisk);
+        }
+
+        private void DrawGraph(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Component is unfinished!", "Unavailable", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+    }
+}
